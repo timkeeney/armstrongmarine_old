071b27ead61c2dbc0715e05562c06d917d977c71
timkeeney <timk@armstrongmarine.com>
2015-06-07 11:32:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Armstrongmarine::Application.config.secret_token = 'b69db60a7c5543c9cc26766ed84d750c5a45182de9ab83ceadd83acc8eaaed9cdcdf9ec7caa13dcd0f1f41d94ed5f03fceb0aacc9535b7947249d63de9f5511e'
