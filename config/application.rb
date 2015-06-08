896c0ee122117988e58e78e46f3bc1329a8bd29e
timkeeney <timk@armstrongmarine.com>
2015-06-07 18:06:44 -0700

    change assets

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,8 @@ module Armstrongmarine
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
+
+    config.assets.precompile += %w( *.js *.css )
   end
 end
